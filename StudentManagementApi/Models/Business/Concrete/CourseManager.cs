f8e1b01550eebca5af152e58b771dd361f45834d
DESKTOP-UP0D5VP\aysek <ayse.klc.95@gmail.com>
2021-05-23 22:41:12 +0300

    edited course, teacher manager clss

---
+++ StudentManagementApi/Models/Business/Concrete/CourseManager.cs
@@ -0,0 +1,18 @@
+﻿using StudentManagementApi.Models.Concrete;
+using StudentManagementApi.Models.DataAccess.Concrete.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagementApi.Models.Business.Concrete
+{
+    public class CourseManager
+    {
+        GenericRepository<Course> repository = new GenericRepository<Course>();
+        public List<Course> GetAllBL()
+        {
+            return repository.List();
+        }
+    }
+}
---
+++ StudentManagementApi/Models/Business/Concrete/TeacherManager.cs
@@ -0,0 +1,18 @@
+﻿using StudentManagementApi.Models.Concrete;
+using StudentManagementApi.Models.DataAccess.Concrete.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentManagementApi.Models.Business.Concrete
+{
+    public class TeacherManager
+    {
+        GenericRepository<Teacher> repository = new GenericRepository<Teacher>();
+        public List<Teacher> GetAllBL()
+        {
+            return repository.List();
+        }
+    }
+}
