73ef9d6c6124013bb657ce73386177d71627fee7
DESKTOP-UP0D5VP\aysek <ayse.klc.95@gmail.com>
2021-05-24 21:40:04 +0300

    added CoursesController

---
+++ StudentManagementApi/Controllers/CoursesController.cs
@@ -0,0 +1,21 @@
+﻿using StudentManagementApi.Models.Business.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentManagementApi.Controllers
+{
+    public class CoursesController : ApiController
+    {
+        CourseManager course = new CourseManager();
+        // GET api/values
+        public IHttpActionResult GetValues()
+        {
+            var coursevalues = course.GetAllBL();
+            return Ok(coursevalues);
+        }
+    }
+}
---
+++ StudentManagementApi/Controllers/StudentsController.cs
@@ -0,0 +1,21 @@
+﻿using StudentManagementApi.Models.Business.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace StudentManagementApi.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        StudentManager student = new StudentManager();
+        // GET api/students
+        public IHttpActionResult GetValues()
+        {
+            var studentvalues = student.GetAllBL();
+            return Ok(studentvalues);
+        }
+    }
+}
